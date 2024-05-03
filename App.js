5e50a051a5f55d86522cbf7f4fbd90e913c84164
soha darvish haghjoo <soha.darvish.haghjoo@gmail.com>
2024-05-03 22:17:42 +0330

    38-end

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-
+import { s } from "./App.style";
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView>
-        <Text>Lets, go</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <>
+      <SafeAreaProvider>
+        <SafeAreaView style={s.app}>
+          <View style={s.header}>
+            <Text>Header</Text>
+          </View>
+          <View style={s.body}>
+            <Text>Body</Text>
+          </View>
+        </SafeAreaView>
+      </SafeAreaProvider>
+      <View style={s.footer}>
+        <Text>Footer</Text>
+      </View>
+    </>
   );
 }
--- App.style.js
+++ App.style.js
@@ -1,3 +1,8 @@
 import { StyleSheet } from "react-native";
 
-export const s = StyleSheet.create({});
+export const s = StyleSheet.create({
+  app: { flex: 1, backgroundColor: "#f1f1f1" },
+  header: { flex: 1 },
+  body: { flex: 5 },
+  footer: { height: 70 },
+});
